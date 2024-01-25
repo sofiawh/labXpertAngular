494831d548b99828ca3a6780b9046848991f0fe6
ayoub ait si ahmad <AITSIAHMAD.AYOUB@ofppt-edu.ma>
2024-01-25 23:40:17 +0100

    i have added this file tailwind.config and some annotation in the global style of the project

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  /**
+   * TO @Sofia AND @Mariam
+   * THIS PLACE WE ADD OUR PATH OF THE TEMPLATE(HTML FILES)
+   */
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
